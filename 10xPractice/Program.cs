a4449f28d03e9dc42b7b1b6b29ce4597e47633e0
Azizbek Ochilov <99602759+azizbek-ochilov@users.noreply.github.com>
2023-02-24 20:52:43 +0500

    Add PresonModel

--- 10xPractice/Program.cs
+++ 10xPractice/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace _10xPractice
+﻿using Classes;
+
+namespace _10xPractice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PersonModel.ServiceRate = 0.12m;
+
+            PersonModel newPerson = new PersonModel()
+            {
+                Name = "Azizbek",
+                Age = 18,
+                CardNumber = "9860 0421 7843 8921"
+            };
+
+            Console.WriteLine(newPerson.CardType);
+            Console.WriteLine(newPerson.Name);
+            Console.WriteLine(newPerson.Age);
+            Console.WriteLine(newPerson.CardNumber);
+            Console.WriteLine(PersonModel.Provider);
+            Console.WriteLine(PersonModel.ServiceRate) ;
+
+            Console.ReadKey();
         }
     }
 }
